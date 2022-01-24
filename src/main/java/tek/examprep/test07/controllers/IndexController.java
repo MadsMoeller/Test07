0c6053dd432db0d1e2e226bda413b960efc215a6
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-24 13:07:39 +0100

    Initial commit

---
+++ src/main/java/tek/examprep/test07/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package tek.examprep.test07.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index.html";
+    }
+
+}
---
+++ src/test/java/tek/examprep/test07/Test07ApplicationTests.java
@@ -0,0 +1,13 @@
+package tek.examprep.test07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
